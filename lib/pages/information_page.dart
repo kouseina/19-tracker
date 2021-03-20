4927bc7850b30c18f1050501d68a583882a03d9c
kouseina <kouseinadaffa@gmail.com>
2021-03-21 06:45:42 +0700

    refactor: information page and setting for build apk

--- lib/pages/information_page.dart
+++ lib/pages/information_page.dart
@@ -42,62 +42,57 @@ class _InformationPageState extends State<InformationPage> {
                     flex: 1,
                     child: ListView(
                       children: infoContents.map((info) {
-                        return Flexible(
-                          child: Container(
-                            margin: EdgeInsets.only(bottom: 20),
-                            child: FlatButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (_) => InformationWebviewPage(
-                                              title: info.title,
-                                              url: info.url,
-                                            )));
-                              },
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    color: Color(0xffF2F2F2),
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(10))),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20, vertical: 25),
-                                  child: Flexible(
-                                    child: Row(
-                                      children: [
-                                        SvgPicture.asset(info.icon),
-                                        SizedBox(
-                                          width: 22,
-                                        ),
-                                        Flexible(
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                info.title,
-                                                style: subtitleStyle.copyWith(
-                                                    fontSize: 15,
-                                                    color: Colors.black,
-                                                    fontWeight:
-                                                        FontWeight.w600),
-                                              ),
-                                              SizedBox(
-                                                height: 5,
-                                              ),
-                                              Text(
-                                                info.desc,
-                                                style: descStyle.copyWith(
-                                                  color: greyColor,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 20),
+                          child: FlatButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => InformationWebviewPage(
+                                            title: info.title,
+                                            url: info.url,
+                                          )));
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Color(0xffF2F2F2),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 25),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset(info.icon),
+                                    SizedBox(
+                                      width: 22,
                                     ),
-                                  ),
+                                    Flexible(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            info.title,
+                                            style: subtitleStyle.copyWith(
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                          SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            info.desc,
+                                            style: descStyle.copyWith(
+                                              color: greyColor,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
